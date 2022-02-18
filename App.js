ebfe6a0f5b8ac6e9d511c7e8d78d31182f6e0b02
Eric Laitman <elaitman1@gmail.com>
2022-02-18 15:32:19 -0800

    Pushing Only Single file to git

--- App.js
+++ App.js
@@ -1,20 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
+// import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import SearchBar from './src/Components/SearchBar'
+import React, {useState} from 'react'
+import useResults from './src/hooks/useResults'
+import ResultsList from './src/Components/ResultsList'
 
 export default function App() {
+const [term, setTerm] = useState('')
+const [searchApi, results, errorMessage] = useResults()
+
+const filtersResultsByPrice = (dollarSign) =>{
+  return results.filter((result) => {
+    return result.price === dollarSign
+  })
+}
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+    <SearchBar 
+      term={term}
+      onTermChange={setTerm}
+      onTermSubmit={() => searchApi(term)} 
+    />
+    {errorMessage ? <Text>{errorMessage}</Text> : null}
+    {/* <Text>We have found {results.length} results</Text> */}
+    <ResultsList reults={filtersResultsByPrice('$')} title='Cost Effective'/>
+    <ResultsList reults={filtersResultsByPrice('$$')} title='Bit Pricier'/>
+    <ResultsList reults={filtersResultsByPrice('$$$')} title='Big Spender'/>
+
     </View>
   );
+
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    borderColor: 'black',
+    height: 40,
+    borderWidth: 3,
+    marginTop: 40
   },
 });
